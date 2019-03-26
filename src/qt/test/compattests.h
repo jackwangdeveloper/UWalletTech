89bfdf16f755caa4d4947c01c04d3b460259b6f4
jackwangdeveloper <jackwangdeveloper@gmail.com>
2019-03-26 11:02:55 +0800

    uwt

---
+++ src/qt/test/compattests.h
@@ -0,0 +1,19 @@
+// Copyright (c) 2009-2018 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef MONOECI_QT_TEST_COMPATTESTS_H
+#define MONOECI_QT_TEST_COMPATTESTS_H
+
+#include <QObject>
+#include <QTest>
+
+class CompatTests : public QObject
+{
+    Q_OBJECT
+
+private Q_SLOTS:
+    void bswapTests();
+};
+
+#endif // MONOECI_QT_TEST_COMPATTESTS_H
